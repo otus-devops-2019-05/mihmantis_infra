459ddab199c663da8f86978c3446753f637979cf
mihmantis <mihmantis@mail.ru>
2019-09-14 19:18:07 +0300

    Fix. Delete additional key for user otus to comply to tests

--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -16,10 +16,6 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable otus_public_key_path {
-  description = "Path to the public key used for ssh access"
-}
-
 variable private_key_path {
   description = "Path to the private key used for ssh access"
 }
