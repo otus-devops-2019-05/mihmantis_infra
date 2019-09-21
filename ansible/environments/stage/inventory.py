110b8b6e43f72c12ef95c3afd82f2182cf9d7117
mihmantis <mihmantis@mail.ru>
2019-09-21 20:58:20 +0300

    Add playbook to create users. Use ansible vault to store user credentials. Dynamic inventory for stage and prod

---
+++ ansible/environments/stage/inventory.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+import subprocess
+from collections import OrderedDict
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--list', dest='list_flag', action='store_true')
+    return parser.parse_args()
+
+
+def main():
+    list_flag = arg_parser().list_flag
+    inventory = {
+        "all": {
+            "children": ["app", "db"] 
+        },
+        "app": {"vars": {}},
+        "db": {"vars": {}},
+        "_meta": {}
+    }
+
+    for gcloud_server_name, server_group in [('reddit-app', 'app'),
+                                             ('reddit-db', 'db')]:
+        cmd = [
+            'gcloud', 'compute', 'instances', 'list', '--filter', 'name:( {} )'.format(gcloud_server_name)
+        ]
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = proc.communicate()
+
+        if output:
+            server_ip = output.splitlines()[1].split()[4].decode()
+            inventory[server_group]['hosts'] = [server_ip]
+
+    if list_flag:
+        print(json.dumps(inventory))
+
+
+if __name__ == '__main__':
+    main()
+
